282e3dd7b56c4f99f6e0ab36cba89b7810e7fff0
Will Foran <willforan@gmail.com>
2021-04-01 13:45:31 -0400

    🚚 organize files

--- gre-mni_registration.bash
+++ gre-mni_registration.bash
@@ -5,11 +5,12 @@ trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
 # 20210401WF - init
 
 # Skullstrip
+test -d bet || mkdir $_
 # "brain extraction" using fsl's bet. could use AFNI's 3dSkullstrip or SPM
-test ! -r t1_bet.nii.gz &&
-   bet T1_MPRAGE_ISO_0006.nii $_
-test ! -r gre_bet.nii.gz &&
-   bet GRE.nii $_
+test ! -r bet/t1_bet.nii.gz &&
+   bet input/T1_MPRAGE_ISO_0006.nii.gz $_
+test ! -r bet/gre_bet.nii.gz &&
+   bet input/GRE.nii.gz $_
 
 ## Regisration
 test -d warps || mkdir $_  # make sure dir exists
@@ -19,17 +20,17 @@ test -d warps || mkdir $_  # make sure dir exists
 
 # t1 <-> mni
 [ -r warps/t1_2_mniWarped.nii.gz ] ||
-   antsRegistrationSyN.sh -d 3 -f MNI_brain.nii.gz -m t1_bet.nii.gz -o warps/t1_2_mni &
+   antsRegistrationSyN.sh -d 3 -f template/MNI152_T1_1mm_brain.nii.gz -m bet/t1_bet.nii.gz -o warps/t1_2_mni &
 
 # gre <-> t1
 # this is rigid (r) transform only 
 # TODO: should we do ridgid+affine?
 [ -r warps/gre_2_t1Warped.nii.gz ] ||
-   antsRegistrationSyN.sh -d 3 -t r -f t1_bet.nii.gz -m gre_bet.nii.tz -o warps/gre_2_t1 &
+   antsRegistrationSyN.sh -d 3 -t r -f bet/t1_bet.nii.gz -m bet/gre_bet.nii.tz -o warps/gre_2_t1 &
 wait
 
 # combine transforms
 # TODO: confrim
-antsApplyTransforms -d 3 -r MNI_brain.nii.gz -i GRE_R2s.nii \
+antsApplyTransforms -d 3 -r MNI_brain.nii.gz -i input/GRE_R2s.nii.gz \
    -t warps/gre_2_t10GenericAffine.mat -t warps/t1_2_mni1InverseWarp.nii.gz \
-   -o GRE_R2s-mni.nii.gz
+   -o out/GRE_R2s-mni.nii.gz
