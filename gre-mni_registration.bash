9d1351423a64239b7a5977a8c439f5c6e6d9015f
Will Foran <willforan@gmail.com>
2021-04-01 14:33:17 -0400

    ⚡️add threading (-n 5); 💡 annotate options

--- gre-mni_registration.bash
+++ gre-mni_registration.bash
@@ -15,23 +15,29 @@ test ! -r bet/gre_bet.nii.gz &&
 ## Regisration
 test -d warps || mkdir $_  # make sure dir exists
 # using ants in two steps. MNI<->T1. GRE<->T1
-# forking both and waiting until they both complete (10 minutes?)
-# default to -t="s" ==>  rigid + affine + deformable syn (3 stages)
+# forking both and waiting until they both complete (30 minutes?)
+# -t="s" (default) ==>  rigid + affine + deformable syn (3 stages)
+# -f is "fixed" source reference
+# -m is "moving" target
+# -n is number of threads
 
 # t1 <-> mni
-[ -r warps/t1_2_mniWarped.nii.gz ] ||
-   antsRegistrationSyN.sh -d 3 -f template/MNI152_T1_1mm_brain.nii.gz -m bet/t1_bet.nii.gz -o warps/t1_2_mni &
+[ -r warps/t1_2_mni_Warped.nii.gz ] ||
+   antsRegistrationSyN.sh -d 3 -f template/MNI152_T1_1mm_brain.nii.gz -m bet/t1_bet.nii.gz -o warps/t1_2_mni_ -n 5 &
 
 # gre <-> t1
-# this is rigid (r) transform only 
-# TODO: should we do ridgid+affine?
-[ -r warps/gre_2_t1Warped.nii.gz ] ||
-   antsRegistrationSyN.sh -d 3 -t r -f bet/t1_bet.nii.gz -m bet/gre_bet.nii.tz -o warps/gre_2_t1 &
+# -t r is rigid transform only
+[ -r warps/gre_2_t1_rigid_Warped.nii.gz ] ||
+   antsRegistrationSyN.sh -d 3 -t r -f bet/t1_bet.nii.gz -m bet/gre_bet.nii.gz -o warps/gre_2_t1_rigid_ -n 5 &
+
+# if we wanted rigid+affine (-t a):
+#[ -r warps/gre_2_t1_affine_Warped.nii.gz ] ||
+#   antsRegistrationSyN.sh -d 3 -t a -f bet/t1_bet.nii.gz -m bet/gre_bet.nii.gz -o warps/gre_2_t1_affine_ &
+
 wait
 
 # combine transforms
 # TODO: confrim warp order produces correct output
-test -d out || mkdir $_
 antsApplyTransforms -d 3 -r template/MNI152_T1_1mm_brain.nii.gz -i input/GRE_R2s.nii.gz \
-   -t warps/gre_2_t10GenericAffine.mat -t warps/t1_2_mni1InverseWarp.nii.gz \
-   -o out/GRE_R2s-mni.nii.gz
+   -t warps/gre_2_t1_rigid_0GenericAffine.mat -t warps/t1_2_mni_Warp.nii.gz \
+   -o warps/GRE_R2s-WarpedMNI.nii.gz
